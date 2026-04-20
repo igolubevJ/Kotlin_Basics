feb23aa8871d5ff8cef375c12064047daaa38955
Ilya Golubev <igolubevapp@gmail.com>
2026-04-20 08:31:56 +0300

    Boolean data type

--- src/Main.kt
+++ src/Main.kt
@@ -1,23 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    // Операция 1. Объединение строк(конкатенация)
-    val typeName = "B2"
-    val serialName = "D" + 2
-    val droidName = typeName + "-" + serialName
-    println(droidName)
+    val isReadySpaceShip = false
+    println("Космический корабль готов? Ответ: " + isReadySpaceShip)
 
-    // Операция 2. Модификация строк
-    val pair = "ПЛАНЕТА: Земля" + "\n" + "СПУТНИК: Луна"
-    val spaceshipName = "КРЕЙСЕР \"Галактика\""
-    println(pair)
-    println(spaceshipName)
+    val isEmptySpaceShip = true
+    println("Космический корабль пустой? Ответ: " + isEmptySpaceShip)
 
-    // Операция 3. Хранение длинных строк
-    val text = """
-        Генерал, в этом сражении Вы одержали победу над 128 дроидами.
-        Это на 17 меньше, чем в битве при Ириандии.
-        Что будете делать?
-    """.trimIndent()
-    println(text)
+    val isActivateLightSaber = true
+    println("Световой меч активен? Ответ: " + isActivateLightSaber)
 }
